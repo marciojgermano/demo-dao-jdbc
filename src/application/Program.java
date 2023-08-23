48900d8ba35ff6a0ac9dab6e1e74bc8d75dd38f5
marciojgermano <marcio.germano@gmail.com>
2023-08-23 18:48:02 -0300

    Implementação de Update

--- src/application/Program.java
+++ src/application/Program.java
@@ -29,11 +29,16 @@ public class Program {
 			System.out.println(obj);
 		}
 		
-		System.out.println("\n==== Test 4: seller Insert ==== ");
+		/*System.out.println("\n==== Test 4: seller Insert ==== ");
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
 		sellerDao.insert(newSeller);
-		System.out.println("Inserted! New id = " + newSeller.getId());
+		System.out.println("Inserted! New id = " + newSeller.getId());*/
 		
+		System.out.println("\n==== TEST 5: seller update ====");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Waine");
+		sellerDao.update(seller);
+		System.out.println("Update completed");
 		
 	}
 }
